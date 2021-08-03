bcff29368ed4f33bff6f8c36a37c243ce90e0cf0
ceglabsagarshinde <52125740+ceglabsagarshinde@users.noreply.github.com>
2021-08-03 18:51:42 +0530

    Add files via upload

---
+++ HYPHYMP_Relax/All_species_Relax/Birds/pseudogene/pseudo_relax.sh
@@ -0,0 +1,53 @@
+for i in *.aln
+do
+sed -e '/^>/s/$/@/' -e 's/^>/#/' $i | tr -d '\n' |sort| tr "#" "\n" | tr "@" "\t"| sed -e 's/^/>/'|sed '1d' -|sed 's/\t/\n/g' > $i.f$
+done
+############################################# do this when we have list of species having pseudogene and functional gene ###########$
+## Create the functional and pseudogene species list manually
+
+for i in `cat birds_functional.txt`
+do
+j=`ls *faa`
+grep -A1 "$i" $j >> functional.fa
+done
+tree=`ls *.nwk`
+for i in `cat birds_pseudogene.txt`
+do
+j=`ls *.faa`
+grep -A1 "$i" $j > $i.seq
+cat functional.fa  >> $i.seq
+grep ">" $i.seq |sed 's/>//g' > list.txt
+echo 'library(ape)' > pruning.r
+echo 'a<-read.tree("'$tree'")' >> pruning.r
+echo 'b<-read.table("list.txt")' >> pruning.r
+echo 'c<-as.character(b$V1)' >> pruning.r
+echo 'd<-keep.tip(a,c)' >> pruning.r
+echo 'write.tree(d,file="'$i.nwk'")' >> pruning.r
+chmod 777 pruning.r
+Rscript pruning.r
+sed -i "s/$i/$i{fg}/g" $i.nwk
+done 
+
+
+
+for i in  `cat  birds_pseudogene.txt`
+do
+seq="$i".seq
+tree="$i".nwk
+echo -ne  "1\n7\n1\n"$PWD"/$seq\n"$PWD"/"$tree"\n2\n2" > "$i"_tree.config
+done
+
+for i in *config
+do
+HYPHYMP  <  $i > $i.out
+done
+
+for j in `ls -1 *out`
+do
+pval=`grep "^Like" $j|awk '{print $6}'|sed 's/\*\*\.//g'`
+kval1=`grep "Relaxation/intensification" $j|awk '{print $6}'|head -1`
+test=`grep "_test_ set:" $j|awk '{print $9}'|sed 's/\`//g'`
+back=`grep "_reference_ set:" $j|cut -f2 -d ":"|sed 's/ //g'`
+echo $test $back $pval $i $kval1 >>birds_nonfunctional_results.txt
+done
+
