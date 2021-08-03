a260264c97fcfa3459fd71fc2caaffee9097638c
ceglabsagarshinde <52125740+ceglabsagarshinde@users.noreply.github.com>
2021-08-03 18:32:33 +0530

    Add files via upload

---
+++ HYPHYMP_Relax/Relax_Galliform/Galliform_relax.sh
@@ -0,0 +1,22 @@
+##making config file for HYPHYMP
+for i in `cat list.directory` 
+do
+seq="$i".aln
+tree="$i".nwk
+echo -ne  "1\n7\n1\n"$PWD"/$seq\n"$PWD"/"$tree"\n2\n2" > "$i"_tree.config
+done
+
+
+##Run the HYPHYMP using config file 
+for j in *config; do HYPHYMP  <  $j > $j.out; done
+
+
+##make single output file 
+for j in `ls -1 *out`
+do
+pval=`grep "^Like" $j|awk '{print $6}'|sed 's/\*\*\.//g'`
+kval1=`grep "Relaxation/intensification" $j|awk '{print $6}'|head -1`
+test=`grep "_test_ set:" $j|awk '{print $9}'|sed 's/\`//g'`
+back=`grep "_reference_ set:" $j|cut -f2 -d ":"|sed 's/ //g'`
+echo $test $back $pval $kval1 >>Galiforms_Relax_results.txt
+done
