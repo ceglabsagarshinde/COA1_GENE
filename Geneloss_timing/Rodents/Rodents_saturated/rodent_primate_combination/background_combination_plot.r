175f1f154efa1e39a22bf4a5e89087d07824cc16
ceglabsagarshinde <52125740+ceglabsagarshinde@users.noreply.github.com>
2021-08-05 19:57:38 +0530

    Add files via upload

---
+++ Geneloss_timing/Rodents/Rodents_saturated/rodent_primate_combination/background_combination_plot.r
@@ -0,0 +1,26 @@
+##This script is to plot the effect of different background species combination on pseudogenization timing estimation.
+#This script is using the output of previous script which compiled the output from all the combinations
+#The initial file required has name outfilecompile
+#You can plot it in JPEG or PDF format. Default is set for JPEG.
+library(ggplot2)
+library(grid)
+require(gridExtra)
+a=read.table(file="outfilecompile",header=T)
+b=a[,c(1,3,4,10,12)]
+jpeg(file="Bg_combination.jpeg", width=1500, height=1250)
+p=list()
+i=1
+#pdf("background_effect.pdf", width=30, height=25)
+for (mdl in as.character(unique(b$model))){
+print(mdl)
+c=b[b$model==mdl,]
+print(head(c))
+theme_set( theme_minimal() + theme(plot.title = element_text(hjust = 0.5),text = element_text(size=15),legend.position = "right",axis.line = element_line(color = "black",size = 0.7, linetype = "solid")))  
+p[[i]]=ggplot(c, aes(x = nBg, y = Tp)) + labs(caption = paste(mdl," nucleotide frequency model", sep="")) + ggtitle("Pseudogenization time estimation using single substitution rate") + geom_line(aes(color = Foreground)) + scale_color_brewer(palette = "Dark2") + scale_x_continuous(breaks=seq(0, 40, 5),name ="Number of Background species (nBg)")+ scale_y_continuous(breaks=seq(0, 21, 3),name ="Time of pseudogenization (MYA)")
+theme_set( theme_minimal() + theme(plot.title = element_text(hjust = 0.5),text = element_text(size=15),legend.position = "right",axis.line = element_line(color = "black",size = 0.7, linetype = "solid")))
+p[[i+1]]=ggplot(c, aes(x = nBg, y = Tp2)) + labs(caption = paste(mdl," nucleotide frequency model", sep="")) + ggtitle("Pseudogenization time estimation using different substitution rate") + geom_line(aes(color = Foreground)) + scale_color_brewer(palette = "Dark2") + scale_x_continuous(breaks=seq(0, 40, 5),name ="Number of Background species (nBg)")+ scale_y_continuous(breaks=seq(0, 21, 3),name ="Time of pseudogenization (MYA)")
+i=i+2
+}
+print(do.call(grid.arrange,p))
+dev.off()
+
---
+++ Geneloss_timing/Rodents/Rodents_saturated/rodent_primate_combination/pruning.r
@@ -0,0 +1,6 @@
+library(ape)
+a<-read.tree("first_clade.nwk")
+b<-read.table("partlist")
+c<-as.character(b$V1)
+d<-keep.tip(a,c)
+write.tree(d,file="part.nwk")
