10949d00d31a862f6a6745e9b8e1ca7174934f36
ceglabsagarshinde <52125740+ceglabsagarshinde@users.noreply.github.com>
2021-08-15 18:22:01 +0530

    Add files via upload

---
+++ HYPHYMP_Relax/All_species_Relax/Feliformia_duplicate/pruning.r
@@ -0,0 +1,6 @@
+library(ape)
+a<-read.tree("Feliformia_duplicate.nwk")
+b<-read.table("Suricata_suricatta2.seq.list")
+c<-as.character(b$V1)
+d<-keep.tip(a,c)
+write.tree(d,file="Suricata_suricatta2.seq.nwk")
