2e65db9ef93a9c311732eddec84ee1113a8d028f
ceglabsagarshinde <52125740+ceglabsagarshinde@users.noreply.github.com>
2021-08-19 12:57:41 +0530

    Delete supplementary_figure_data/supplementaryGC_vs_kmer_plot directory

--- supplementary_figure_data/supplementaryGC_vs_kmer_plot/Readme.md
+++
@@ -1,3 +0,0 @@
-## GC Vs. kmer plot 
-The plotting_script.r is used to plot the GC Vs. kmer abundance for different species. The input files are generated using getGC.sh script. The input files, script and result figure is provided in this folder.
-We used different sequencing plateform data.
--- supplementary_figure_data/supplementaryGC_vs_kmer_plot/getGC.sh
+++
@@ -1,6 +0,0 @@
-## the input is the database fasta file. The *_counts output file of the last step is used for generating the plot.
-i=$1
-jellyfish count -m 21 -s 1000M -t 10 -C $i -o "$i"_jell
-jellyfish dump "$i"_jell > "$i"_jell_counts_dumps.fa
-seqkit fx2tab --name --gc "$i"_jell_counts_dumps.fa > "$i"_jell_gc_counts.txt
-paste "$i"_jell_counts_dumps.fa "$i"_jell_gc_counts.txt|awk '{printf "%.0f %.2f\n", $2,$3}'|awk '{a[$2] += $1} END{for (i in a) print i, a[i]}' > "$i"_jell_gc_counts
