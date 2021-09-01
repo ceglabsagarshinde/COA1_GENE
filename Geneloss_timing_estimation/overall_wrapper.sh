ddc8565e3e74726f450cd74a17fb6df503959557
ceglabsagarshinde <52125740+ceglabsagarshinde@users.noreply.github.com>
2021-09-01 15:58:23 +0530

    Add files via upload

---
+++ Geneloss_timing_estimation/overall_wrapper.sh
@@ -0,0 +1,14 @@
+##This is the overall wrapper script for gene loss time estimation in different species
+##Each directory has their wrapper script inside. This script run those scripts, compile the results and generate the figure. Each wrapper script can be run individually also if one wishes to get the result of only one group.
+for dir in `ls -d */|sed 's/\///g'`
+do
+cd $dir
+for script in `ls -1 *sh`
+do
+sh $script
+done
+cd ..
+done
+
+sh compiling_script.sh
+Rscript Pseudogenization_time_compile.r
