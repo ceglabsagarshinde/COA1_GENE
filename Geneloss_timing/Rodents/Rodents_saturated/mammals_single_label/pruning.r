0b0b97ee6533dea84d8ef3c68dd6ac48c7ae49fd
ceglabsagarshinde <52125740+ceglabsagarshinde@users.noreply.github.com>
2021-09-01 19:01:23 +0530

    Delete Geneloss_timing/Rodents directory

--- Geneloss_timing/Rodents/Rodents_saturated/mammals_single_label/pruning.r
+++
@@ -1,6 +0,0 @@
-library(ape)
-a<-read.tree("rodents.nwk")
-b<-read.table("list.txt")
-c<-as.character(b$V1)
-d<-keep.tip(a,c)
-write.tree(d,file="Oryctolagus_cuniculus.nwk")
--- Geneloss_timing/Rodents/Rodents_saturated/multiple_label.sh
+++
@@ -1,8 +0,0 @@
-for dir in mammals_multiple_labels Rodents_multiple_labels
-do
-cd $dir
-rm codeml.ctl
-./Tp_estimation_multiple_label.sh
-cd ..
-done
-
