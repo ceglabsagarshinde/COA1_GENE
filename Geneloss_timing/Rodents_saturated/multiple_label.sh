d28aed1779907c99415b98da6fed24dc1a07a6c9
ceglabsagarshinde <52125740+ceglabsagarshinde@users.noreply.github.com>
2021-10-04 15:20:25 +0530

    Update multiple_label.sh

--- Geneloss_timing/Rodents_saturated/multiple_label.sh
+++ Geneloss_timing/Rodents_saturated/multiple_label.sh
@@ -2,7 +2,8 @@ for dir in mammals_multiple_labels Rodents_multiple_labels
 do
 cd $dir
 rm codeml.ctl
-./Tp_estimation_multiple_label.sh
+chmod 777 Tp_estimation_multiple_label.sh
+bash Tp_estimation_multiple_label.sh
 cd ..
 done
 
