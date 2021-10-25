a29a5aaf4ccc32e2467506c97800884692ce2935
ceglabsagarshinde <52125740+ceglabsagarshinde@users.noreply.github.com>
2021-10-25 10:39:43 +0530

    Add files via upload

---
+++ Geneloss_timing/Galliformes/C_pictus_P_colchicus_Combinations/pictus_colchicus_both_functional/splittime.r
@@ -0,0 +1,7 @@
+library(ape)
+library(reshape2)
+a<-read.tree("galliforms_functional_labelled.nwk")
+cophenetic(a)->v
+subset(melt(v), value!=0)->v1
+v1$value= round(v1$value/2,digit=2)
+write.table(v1,file="pairwise.txt",quote=F,sep="\t",row.names=F,col.names=F)
