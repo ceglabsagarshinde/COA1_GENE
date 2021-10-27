a09dfabdca89b6fec0ac2d26290edc0f2ee83ef2
ceglabsagarshinde <52125740+ceglabsagarshinde@users.noreply.github.com>
2021-10-27 09:52:09 +0530

    Add files via upload

---
+++ supplementary_figure_data/supplementaryGC_vs_kmer_plot/getGC.sh
@@ -0,0 +1,6 @@
+i=$1
+jellyfish count -m 21 -s 1000M -t 10 -C $i -o "$i"_jell
+jellyfish dump "$i"_jell > "$i"_jell_counts_dumps.fa
+seqkit fx2tab --name --gc "$i"_jell_counts_dumps.fa > "$i"_jell_gc_counts.txt
+paste "$i"_jell_counts_dumps.fa "$i"_jell_gc_counts.txt|awk '{printf "%.0f %.2f\n", $2,$3}'|awk '{a[$2] += $1} END{for (i in a) print i, a[i]}' > "$i"_jell_gc_counts
+
