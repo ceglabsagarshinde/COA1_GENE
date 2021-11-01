edd67cded910ff689c9db6e61d0c17e9f156ee03
ceglabsagarshinde <52125740+ceglabsagarshinde@users.noreply.github.com>
2021-11-01 18:40:24 +0530

    Add files via upload

--- supplementary_figure_data/Woolly_mammoth_databases_comparison/g/script.r
+++ supplementary_figure_data/Woolly_mammoth_databases_comparison/g/script.r
@@ -6,7 +6,7 @@ jpeg("GC_abundance.jpeg",width=2400,height=800)
 par(mai=c(1.5,1.5,0.5,0.5))
 read.table(file="PRJEB7929_ERR852028_ERR855944.fa_jell_gc_counts",header=F)->M
 M[order(M$V1), ]->N
-plot(N$V1,N$V2/sum(N$V2),type="b",ylim=c(0,0.3),xlab="GC percent",ylab="Kmer abundance",cex.lab=4,cex.axis=2,lwd=5)
+plot(N$V1,N$V2/sum(N$V2),type="b",ylim=c(0,0.3),xlab="GC percent",ylab="k-mer abundance",cex.lab=4,cex.axis=2,lwd=5)
 
 read.table(file="PRJDB4697_DRR058709_14.fa_jell_gc_counts",header=F)->M
 M[order(M$V1), ]->N
